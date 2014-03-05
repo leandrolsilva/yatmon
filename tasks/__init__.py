477a39109abd8836692014262e1cc78ba7ffc360
Alexandre Souza <aesouza.jvm@gmail.com>
2014-03-05 03:34:25 -0300

    cleaning the project to dev. using TDD

--- tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-from core import app
-
-def register_blueprints(app):
-    #prevents circular imports
-    from tasks.views import tasks
-    app.register_blueprint(tasks)
-
-register_blueprints(app)
