6e0b2bfc6340455fe2a8bcb700fcc70026efc8a4
Alexandre Souza <aesouza.jvm@gmail.com>
2014-03-08 06:22:10 -0300

    adding angularJS and task class to model.py

--- app/models.py
+++ app/models.py
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 *-*
-#
+from app import db
+
+
+class Task(db.Document):
+    name = db.StringField(required=True, max_length=50)
+    #exec_date = DateTimeField(required=True)
+    priority = db.ListField(db.StringField(max_length=30, default="low"))
+    email_alert = db.StringField(required=True, max_length=50)
+    description = db.StringField(required=True, max_length=255)
+
+    def __unicode__(self):
+        return self.name
