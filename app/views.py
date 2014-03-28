95ba290d87534fd9c64687ea451984ecd7d40d52
Alexandre Souza <aesouza.jvm@gmail.com>
2014-03-28 13:12:09 -0300

    a little joke for source code link

--- app/views.py
+++ app/views.py
@@ -1,11 +1,21 @@
 from flask import render_template, redirect
 from app import app
-
+import urllib.request
 
 @app.route('/project/')
 def project():
-    return redirect("https://github.com/alesouza/yatmon.git")
-
+    '''
+    if github down (yeap...), try redirect to bitbucket...
+    '''
+    try:
+        github = "https://github.com/alexandre/yatmon.git"
+        bitbucket = "https://bitbucket.org/alesouza/yatmon"
+        if urllib.request.urlopen(github).getcode() != 200:
+	        return redirect(bitbucket)
+        return redirect(github)
+    except Exception as error:         
+        return "mayday...mayday....houston, we have a problem! WTH!, the source code is lost..."
+                
 
 @app.route('/')
 @app.route('/index/')
